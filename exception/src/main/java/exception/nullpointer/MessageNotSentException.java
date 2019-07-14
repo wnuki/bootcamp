c4bddedbc2f527612d592f9a6bed9c073c97a967
Kinga Wnuk <kinga.wnuk@protonmail.com>
2019-07-14 14:25:02 +0200

    (exception) have NullPointerException submodule added

---
+++ exception/src/main/java/exception/nullpointer/MessageNotSentException.java
@@ -0,0 +1,7 @@
+package exception.nullpointer;
+
+public class MessageNotSentException extends Exception {
+    public MessageNotSentException(final String message) {
+        super(message);
+    }
+}
---
+++ exception/src/main/java/exception/nullpointer/NullPointerExceptionRunner.java
@@ -0,0 +1,13 @@
+package exception.nullpointer;
+
+public class NullPointerExceptionRunner {
+    public static void main(String[] args) {
+        User user = null;
+        MessageSender messageSender = new MessageSender();
+        try {
+            messageSender.sendMessageTo(user, "Hello!");
+        } catch (MessageNotSentException e) {
+            System.out.println("Message is not send, but program is still running");
+        }
+    }
+}
